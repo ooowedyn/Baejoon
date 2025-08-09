12599e3c7cad58481d03d275601d3153b6fedd01
ooowedyn <ooowedyn@naver.com>
2025-08-09 21:55:59 +0900

    [Bronze V] Title: 나머지, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.py
@@ -0,0 +1,8 @@
+#(A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C
+A, B, C = input().split()
+A, B, C = int(A),int(B),int(C)
+
+print((A+B)%C)
+print(((A%C) + (B%C))%C)
+print((A*B)%C)
+print(((A%C) * (B%C))%C)
