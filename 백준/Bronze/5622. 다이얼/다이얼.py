1246bdbde6d5b3fe46d3b57a480c85abc7e172e8
ooowedyn <ooowedyn@naver.com>
2025-09-10 17:13:39 +0900

    [Bronze II] Title: 다이얼, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,13 @@
+import sys
+
+s = sys.stdin.readline().strip()
+
+groups = ["ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"]
+time = 0
+for ch in s:
+    for i, g in enumerate(groups):
+        if ch in g:
+            time += i + 3
+            break
+
+print(time)
